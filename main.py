13c656304e94d5ef981a410ac9fa6552a2b37148
Coda Bool <codabool@pm.me>
2022-12-07 20:21:59 -0500

    output to a single json file

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import json
 # import pandas as pd
 from pathlib import Path
 
-append_tasks = True
-write_results = False
 path = "./data"
 out_dir = "./results/"
 dir_list = os.listdir(path)
-
+# There are too many leaked_logcat_data_ to select for all of them
+# I only grab 4 here
 list_of_relevant_findings = [
   'zip_file_in_transit_check_broken_ssl',
   'broken_ssl',
@@ -16,7 +15,7 @@ list_of_relevant_findings = [
   'sensitive_data_http_bluetooth_mac',
   'leaked_data_in_files_bluetooth_mac',
   'leaked_logcat_data_bluetooth_mac',
-  'leaked_logcat_data_build_fingerprint',
+  'leaked_logcat_data_build_fingerprint', 
   'leaked_logcat_data_android_id',
   'leaked_logcat_data_dns1',
   'sensitive_data_http_custom',
@@ -28,6 +27,7 @@ list_of_relevant_findings = [
   'writable_executable_files_check',
 ]
 
+main_output = []
 for file_name in os.listdir(path):
   print("==>", file_name)
   try:
@@ -39,8 +39,8 @@ for file_name in os.listdir(path):
     print("     ", app['title'])
     obj = {
       'title': app['title'],
+      'file': file_name,
       'publisher': app['assessment']['publisherName'],
-      'platform': app['assessment']['platformType'],
       'score': app['assessment']['report']['score']
     }
     # Check if the app is a game
@@ -52,42 +52,47 @@ for file_name in os.listdir(path):
         'puzzle' in app['title'].lower() or
         'puzzle' in obj['publisher'].lower()):
       is_game = True
-    if append_tasks:
-      obj['tasks'] = []
-      permissions = None
-      urls = None
-      for task_type, value in app['assessment']['analysis']['task'].items():
-        if value is None:
+    obj['tasks'] = []
+    permissions = None
+    urls = None
+    for task_type, value in app['assessment']['analysis']['task'].items():
+      if value is None:
+        continue
+      result_location = value['result']
+      if task_type == 'yaapStatic':
+        result_location = value['result'][0]
+      for task, data in result_location.items():
+        task_obj = {}
+        if not data:
           continue
-        result_location = value['result']
-        if task_type == 'yaapStatic':
-          result_location = value['result'][0]
-        for task, data in result_location.items():
-          task_obj = {}
-          if not data:
-            continue
-          if type(data) == list:
-            task_obj['data'] = data[0] 
-          else:
-            task_obj['data'] = data   
-          task_obj['type'] = task_type
-          task_obj['task'] = task
-          obj['tasks'].append(task_obj)
+        if type(data) == list:
+          task_obj['data'] = data[0] 
+        else:
+          task_obj['data'] = data   
+        task_obj['type'] = task_type
+        task_obj['task'] = task
+        obj['tasks'].append(task_obj)
 
-          # Permissions
-          if task == "myall":
-            permissions = data['manifest']['print_permissions']['total']
-          # Urls
-          if task == "yaap_data":
-            urls = data['urls']['info']['urls']['data']
+        # Permissions
+        if task == "myall":
+          permissions = data['manifest']['print_permissions']['total']
+        # Urls
+        if task == "yaap_data":
+          urls = data['urls']['info']['urls']['data']
 
     # Permissions
     if permissions:
       print("       ", permissions, 'permissions')
+      obj['permissions'] = permissions
     if urls:
       print("       ", len(urls), 'urls')
+      # change this to 
+      # obj['urls'] = urls
+      # if you want the full list of urls
+      obj['urls'] = len(urls)
 
     # Game
+    obj['game'] = is_game
     if is_game:
       print("        is a game")
     else:
@@ -98,15 +103,23 @@ for file_name in os.listdir(path):
 
     # Find specific findings
     print("        vulnerabilites")
+    obj['targets'] = []
     for find in app['assessment']['report']['findings']:
       for check in list_of_relevant_findings:
+        # There are too many leaked_logcat_data_ to select for all of them
+        # if 'leaked_logcat_data_' in check or check == find['checkId'] and find['affected']:
         if check == find['checkId'] and find['affected']:
           print("         -", check)
+          obj['targets'].append(check)
       
     output.append(obj)
+
+  # Trim output
+  for app in output:
+    del app['tasks']
+    del app['findings']
+  main_output.append(output[0])
       
-  if write_results:
-    print(len(urls))
-    json_obj = json.dumps(output, indent=2)
-    with open(out_dir + file_name[:8]  + "-format.json", "w") as outfile:
-      outfile.write(json_obj)
+json_obj = json.dumps(main_output, indent=2)
+with open(out_dir + "main.json", "w") as outfile:
+  outfile.write(json_obj)
